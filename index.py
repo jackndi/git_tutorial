3ff335d43151a0c540b289ea1f9f8a705320c4b1
Jackson Kabuku <ndirangu240@gmail.com>
2020-10-03 13:21:10 +0200

    Revert "implemented is eve iii n"
    This reverts commit 0b3f2c6f32935a177e9684b82c6893688d7d0dc4.

--- index.py
+++ index.py
@@ -4,16 +4,9 @@ def main():
     print(" Main function ")
 
 
-def is_even(number):
-    """ tells us if the no. is even"""
-    return True if number % 2 == 0 else False
-
-
 if __name__ == "__main__":
     main()
 
-    print(" 1 ", is_even(1))
-    print(" 4 ", is_even(4))
 
 # open terminal on vs-code
 # Ctrl + `
